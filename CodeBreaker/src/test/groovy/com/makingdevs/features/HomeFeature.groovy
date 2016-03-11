c4a241cc8318d9c112241d46e2a0792171bbf02e
neodevelop <juan@makingdevs.com>
2016-03-10 20:58:28 -0600

    Haciendo pruebas de Cucumber con Groovy

--- CodeBreaker/src/test/groovy/com/makingdevs/features/HomeFeature.groovy
+++ CodeBreaker/src/test/groovy/com/makingdevs/features/HomeFeature.groovy
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = { "classpath:features/welcome.feature" }, strict = true, plugin = { "html:build/test-report" })
+@CucumberOptions(features = [ "classpath:features/welcome.feature" ], strict = true, plugin = [ "html:build/test-report" ])
 public class HomeFeature {
 
 }
---
+++ CodeBreaker/src/test/groovy/com/makingdevs/features/HomeSteps.groovy
@@ -0,0 +1,5 @@
+package com.makingdevs.features
+
+this.metaClass.mixin(cucumber.api.groovy.Hooks)
+this.metaClass.mixin(cucumber.api.groovy.EN)
+
